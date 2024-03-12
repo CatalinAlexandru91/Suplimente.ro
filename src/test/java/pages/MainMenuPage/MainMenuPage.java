4911ba1d1f9155436eee32045bed95af5ea981eb
CatalinAlexandru91 <alex.tarachiu91@gmail.com>
2024-03-12 10:02:38 +0200

    MainMenuPage and MainMenuTest

--- src/test/java/pages/MainMenuPage/MainMenuPage.java
+++ src/test/java/pages/MainMenuPage/MainMenuPage.java
@@ -26,6 +26,34 @@ public class MainMenuPage extends BasePage {
     private By buyButton = By.xpath("//a[@onclick=\"addToCart('24106', '1');\"]");
     private By categoriesButton = By.xpath("//span[@class='main-menu-text' and text()='CATEGORII PRODUSE']\n");
     private By clickVitaminsOption = By.xpath("//a[text()='VITAMINE']\n");
+    private By clickMultivitaminsOption = By.xpath("//span[@class='sf-name' and text()='Multivitamine (15)']\n");
+    private By selectByProducer = By.xpath("//span[@class='sf-name' and text()='Universal Nutrition (2)']\n");
+    private By buyButton2 = By.xpath("//a[@onclick=\"addToCart('22497', '1');\"]");
+    private By categoriesButton2 = By.xpath("//span[@class='main-menu-text' and text()='CATEGORII PRODUSE']\n");
+    private By clickMineralsOption = By.xpath("//a[text()='MINERALE']\n");
+    private By clickMultimineralsOption = By.xpath("//span[@class='sf-name' and text()='Multiminerale (10)']\n");
+    private By selectProducer2 = By.xpath("//span[@class=\"sf-name\" and contains(text(), \"Nutrend (2)\")]\n");
+    private By buyButton3 = By.xpath("//a[@onclick=\"addToCart('24165', '1');\"]");
+    private By generalHealthButton = By.xpath("//span[@class=\"main-menu-text\" and contains(text(), \"SANATATE GENERALA\")]\n");
+    private By liverAid = By.xpath("//span[@class='sf-name' and text()='Hepatoprotectoare (21)']");
+    private By selectProducer3 = By.xpath("//span[@class='sf-name' and text()='Scitec (1)']");
+    private By buyButton4 = By.xpath("//a[@onclick=\"addToCart('22626', '1');\"]");
+    private By antiOxidants = By.xpath("//a[text()='Antioxidanti']");
+    private By selectProducer4 = By.xpath("//span[@class=\"sf-name\" and contains(text(), \"Olimp Nutrition (1)\")]\n");
+    private By buyButton5 = By.xpath("//a[@onclick=\"addToCart('23779', '1');\"]");
+    private By sportsNutrition = By.xpath("//span[@class=\"main-menu-text\" and text()=\"NUTRITIE SPORTIVA\"]\n");
+    private By creatineOption = By.xpath("//a[text()='Creatina']");
+    private By selectProducer5 = By.xpath("//span[@class=\"sf-name\" and text()=\"Dymatize (2)\"]");
+    private By buyButton6 = By.xpath("//a[@onclick=\"addToCart('22577', '1');\"]");
+    private By sportsNutrition2 = By.xpath("//span[@class=\"main-menu-text\" and text()=\"NUTRITIE SPORTIVA\"]\n");
+    private By protein = By.xpath("//a[text()='Proteine']");
+    private By selectProducer6 = By.xpath("//span[@class=\"sf-name\" and contains(text(), \"Optimum Nutrition (6)\")]\n");
+    private By selectFlavour = By.xpath("//span[@class=\"sf-name\" and contains(text(), \"Strawberry (1)\")]\n");
+    private By buyButton7 = By.xpath("//a[@onclick=\"addToCart('22603', '1');\"]");
+    private By producersButton = By.xpath("//span[@class=\"main-menu-text\" and text()=\"PRODUCĂTORI\"]\n");
+    private By universalNutrition = By.xpath("//a[text()='Universal Nutrition']");
+    private By producersButton2 = By.xpath("//span[@class=\"main-menu-text\" and text()=\"PRODUCĂTORI\"]\n");
+    private By optimumNutrition = By.xpath("//a[text()='Optimum Nutrition']");
 
     public void clickPromotionsButton() {
         LOG.info("Click 'Promotions' button");
@@ -57,6 +85,7 @@ public class MainMenuPage extends BasePage {
     }
 
     public void clickCategoriesOption() {
+        sleep(3000L);
         LOG.info("CLick 'Categories' option");
         driver.findElement(categoriesButton).click();
     }
@@ -65,5 +94,172 @@ public class MainMenuPage extends BasePage {
         sleep(3000L);
         LOG.info("Click 'Vitamins' option");
         driver.findElement(clickVitaminsOption).click();
+    }
+
+    public void clickMultivitaminsOption() {
+        LOG.info("Click 'Multivitamins' option");
+        driver.findElement(clickMultivitaminsOption).click();
+
+    }
+
+    public void clickByProducer() {
+        LOG.info("Click 'Select by producer' option");
+        driver.findElement(selectByProducer).click();
+
+    }
+
+    public void clickBuyButton2() {
+        sleep(3000L);
+        LOG.info("Click 'Buy' button");
+        driver.findElement(buyButton2).click();
+
+
+    }
+
+
+    public void clickCategoriesOption2() {
+        LOG.info("CLick 'Categories' option");
+        driver.findElement(categoriesButton2).click();
+
+    }
+
+
+    public void clickMineralsOption() {
+        sleep(3000L);
+        LOG.info("Click 'Minerals' option");
+        driver.findElement(clickMineralsOption).click();
+
+    }
+
+
+    public void clickMultiMineralsOption() {
+        sleep(3000L);
+        LOG.info("Click 'Multiminerals' option");
+        driver.findElement(clickMultimineralsOption).click();
+
+    }
+
+    public void clickselectProducerButton2() {
+        LOG.info("Click 'Select producer' button");
+        driver.findElement(selectProducer2).click();
+
+    }
+
+
+    public void clickBuyButton3() {
+        LOG.info("Click 'Buy' button");
+        driver.findElement(buyButton3).click();
+
+
+    }
+
+    public void clickGeneralHealthButton() {
+        sleep(3000L);
+        LOG.info("Click 'General health' button");
+        driver.findElement(generalHealthButton).click();
+
+
+    }
+
+    public void clickLiverAidOption() {
+        sleep(3000L);
+        LOG.info("Click 'Liver aid' option");
+        driver.findElement(liverAid).click();
+
+    }
+
+    public void clickProducerButton3() {
+        LOG.info("Click 'Select producer' button");
+        driver.findElement(selectProducer3).click();
+
+    }
+
+    public void clickBuyButton4() {
+        LOG.info("Click 'Buy' button");
+        driver.findElement(buyButton4).click();
+
+
+    }
+
+    public void clickAntioxidantsOption() {
+        LOG.info("Click 'Antioxidants' option");
+        driver.findElement(antiOxidants).click();
+
+
+    }
+
+    public void clickProducerButton4() {
+        LOG.info("Click 'Select producer' button");
+        driver.findElement(selectProducer4).click();
+
+    }
+
+    public void clickBuyButton5() {
+        LOG.info("Click 'Buy' button");
+        driver.findElement(buyButton5).click();
+
+    }
+
+    public void clickSportsNutritionOption() {
+        LOG.info("Click 'Sports nutrition' option");
+        driver.findElement(sportsNutrition).click();
+
+    }
+
+    public void clickProducerButton5() {
+        LOG.info("Click 'Select producer' button");
+        driver.findElement(selectProducer5).click();
+
+    }
+
+    public void clickBuyButton6() {
+        LOG.info("Click 'Buy' button");
+        driver.findElement(buyButton6).click();
+    }
+
+    public void clickSportsNutritionOption2() {
+        LOG.info("Click 'Sports nutrition' option");
+        driver.findElement(sportsNutrition2).click();
+    }
+
+    public void clickProducerButton6() {
+        LOG.info("Click 'Select producer' button");
+        driver.findElement(selectProducer6).click();
+
+    }
+
+    public void selectFlavour() {
+        LOG.info("Click 'Flavour' button");
+        driver.findElement(selectFlavour).click();
+
+    }
+
+    public void clickBuyButton7() {
+        LOG.info("Click 'Buy' button");
+        driver.findElement(buyButton7).click();
+    }
+
+    public void clickProducersButton() {
+        LOG.info("Click 'Producers' button");
+        driver.findElement(producersButton).click();
+
+    }
+
+    public void clickUniversalNutrition() {
+        LOG.info("Click 'Universal Nutrition' button");
+        driver.findElement(universalNutrition).click();
+
+    }
+
+    public void clickProducersButton2() {
+        LOG.info("Click 'Producers' button");
+        driver.findElement(producersButton2).click();
+
+    }
+
+        public void clickOptimumNutrition() {
+        LOG.info("Click 'Optimum Nutrition' button");
+        driver.findElement(optimumNutrition).click();
+
     }
 }
--- src/test/java/tests/MainMenuTest/MainMenuTest.java
+++ src/test/java/tests/MainMenuTest/MainMenuTest.java
@@ -40,6 +40,83 @@ public class MainMenuTest extends BaseTest {
         LOG.info("Click 'Vitamins' option");
         mainMenuPage.clickVitaminsOption();
 
+        LOG.info("Click 'Multivitamins' option");
+        mainMenuPage.clickMultivitaminsOption();
+
+        LOG.info("Click 'Select by producer' option");
+        mainMenuPage.clickByProducer();
+
+        LOG.info("Click 'Buy' button");
+        mainMenuPage.clickBuyButton2();
+
+        LOG.info("Click 'Categories' option");
+        mainMenuPage.clickCategoriesOption2();
+
+        LOG.info("Click 'Minerals' option");
+        mainMenuPage.clickMineralsOption();
+
+        LOG.info("Click 'Multiminerals' option");
+        mainMenuPage.clickMultiMineralsOption();
+
+        LOG.info("Click 'Select producer' button");
+        mainMenuPage.clickselectProducerButton2();
+
+        LOG.info("Click 'Buy' button");
+        mainMenuPage.clickBuyButton3();
+
+        LOG.info("Click 'General health' button");
+        mainMenuPage.clickGeneralHealthButton();
+
+        LOG.info("Click 'Liver aid' option");
+        mainMenuPage.clickLiverAidOption();
+
+        LOG.info("Click 'Select producer' button");
+        mainMenuPage.clickProducerButton3();
+
+        LOG.info("Click 'Buy' button");
+        mainMenuPage.clickBuyButton4();
+
+        LOG.info("Click 'Antioxidants' option");
+        mainMenuPage.clickAntioxidantsOption();
+
+        LOG.info("Click 'Select producer' button");
+        mainMenuPage.clickProducerButton4();
+
+        LOG.info("Click 'Buy' button");
+        mainMenuPage.clickBuyButton5();
+
+        LOG.info("Click 'Sports nutrition' option");
+        mainMenuPage.clickSportsNutritionOption();
+
+        LOG.info("Click 'Select producer' button");
+        mainMenuPage.clickProducerButton5();
+
+        LOG.info("Click 'Buy' button");
+        mainMenuPage.clickBuyButton5();
+
+        LOG.info("Click 'Sports nutrition' option");
+        mainMenuPage.clickSportsNutritionOption2();
+
+        LOG.info("Click 'Select producer' button");
+        mainMenuPage.clickProducerButton6();
+
+        LOG.info("Click 'Flavour' button");
+        mainMenuPage.selectFlavour();
+
+        LOG.info("Click 'Buy' button");
+        mainMenuPage.clickBuyButton7();
+
+        LOG.info("Click 'Producers' button");
+        mainMenuPage.clickProducersButton();
+
+        LOG.info("Click 'Universal Nutrition' button");
+        mainMenuPage.clickUniversalNutrition();
+
+        LOG.info("Click 'Producers' button");
+        mainMenuPage.clickProducersButton2();
+
+        LOG.info("Click 'Optimum Nutrition' button");
+        mainMenuPage.clickOptimumNutrition();
 
 
 
